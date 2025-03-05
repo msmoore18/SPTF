bb00792dd72a584a2f8ae738a9065caf2d694c5c
msmoore18 <slater.meagan@gmail.com>
2025-03-05 15:33:20 -0700

    Update test1.py

--- test1.py
+++ test1.py
@@ -51,8 +51,7 @@ with col1:
     height_group = filtered_data.groupby(["Tree Height (ft)", "Quality"])["Count"].sum().reset_index()
     height_group["Total Height"] = height_group.groupby("Tree Height (ft)")["Count"].transform("sum")
     fig = px.bar(height_group, x='Tree Height (ft)', y='Count', color='Quality', barmode='stack', hover_data={'Tree Height (ft)': True, 'Count': True, 'Total Height': True}, 
-                 labels={'Tree Height (ft)': 'Tree Height (ft)', 'Count': 'Tree Count'}, 
-                 hover_data={'Tree Height (ft)': True, 'Count': True})
+                 labels={'Tree Height (ft)': 'Tree Height (ft)', 'Count': 'Tree Count'})
     st.plotly_chart(fig)
 
         # Bar Chart: Tree Count vs Row (only if one lot is selected)
