277e2324fd5df2ef928629a82a258ce0be0b29d6
msmoore18 <slater.meagan@gmail.com>
2025-03-05 15:41:19 -0700

    Update test1.py

--- test1.py
+++ test1.py
@@ -51,8 +51,8 @@ with col1:
     height_group = filtered_data.groupby("Tree Height (ft)")["Count"].sum()
     
     fig = px.bar(height_group.reset_index(), x='Tree Height (ft)', y='Count', hover_data={'Tree Height (ft)': True, 'Count': True}, category_orders={'Tree Height (ft)': sorted(data['Tree Height (ft)'].unique())}, labels={'Tree Height (ft)': 'Tree Height (ft)', 'Count': 'Tree Count'})
-    st.plotly_chart(fig)
     fig.update_layout(xaxis=dict(tickmode='linear'))
+    st.plotly_chart(fig)
 
         # Bar Chart: Tree Count vs Row (only if one lot is selected)
     if len(lots) == 1:
@@ -65,4 +65,5 @@ with col1:
 with col2:
     # Display Image
     st.image("SPTF.png", width=375)
+  
 
