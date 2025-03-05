d17cb24347a5379c11e42d419d2183543ce9e620
msmoore18 <slater.meagan@gmail.com>
2025-03-05 16:54:39 -0700

    Update test1.py

--- test1.py
+++ test1.py
@@ -80,7 +80,7 @@ elif page == "Rudy's Page":
 
     with col1:
         # Bar Chart: Tree Count vs Row (Stacked by Quality)
-        st.write(f"### Lot {lots} Tree Height")
+        st.write(f"### Lot {lots} Tree Count")
         row_group = filtered_data.groupby(["Row", "Quality"])["Count"].sum().reset_index()
         fig = px.bar(row_group, x='Row', y='Count', color='Quality', barmode='stack', labels={'Row': 'Row', 'Count': 'Tree Count', 'Quality': 'Quality'}, hover_data={'Row': True, 'Count': True, 'Quality': True})
         fig.update_layout(xaxis=dict(tickmode='linear'))
