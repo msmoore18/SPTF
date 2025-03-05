a6818784a7a6b373072a524dc0b091fdeee8b3e1
msmoore18 <slater.meagan@gmail.com>
2025-03-05 15:30:59 -0700

    Update test1.py

--- test1.py
+++ test1.py
@@ -48,8 +48,8 @@ col1, col2 = st.columns([4, 2])
 with col1:
     # Bar Chart: Tree Count vs Tree Height
     st.write("### Tree Count vs Tree Height")
-    height_group = filtered_data.groupby("Tree Height (ft)")["Count"].sum()
-    fig = px.bar(height_group.reset_index(), x='Tree Height (ft)', y='Count', 
+    height_group = filtered_data.groupby(["Tree Height (ft)", "Quality"])["Count"].sum().reset_index()
+    fig = px.bar(height_group, x='Tree Height (ft)', y='Count', color='Quality', barmode='stack', 
                  labels={'Tree Height (ft)': 'Tree Height (ft)', 'Count': 'Tree Count'}, 
                  hover_data={'Tree Height (ft)': True, 'Count': True})
     st.plotly_chart(fig)
@@ -65,4 +65,3 @@ with col1:
 with col2:
     # Display Image
     st.image("SPTF.png", width=375)
-
