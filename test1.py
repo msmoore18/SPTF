6e1e79383e56367dbeb9480e0dc4059e62f9cd1a
msmoore18 <slater.meagan@gmail.com>
2025-03-05 21:03:57 -0700

    Update test1.py

--- test1.py
+++ test1.py
@@ -86,7 +86,7 @@ if page == "Tree Inventory":
 
     
 
-. Use the filters on the left to select which Quality (B: Needs Pruning, C: Needs to be cut), Lot #, and Tree Height you want displayed in the table")
+
     
     # Filtered summary of trees
     tree_summary = filtered_data.groupby(["Quality", "Lot", "Row", "Tree Height (ft)"])['Count'].sum().reset_index()
@@ -100,7 +100,7 @@ if page == "Tree Inventory":
         file_name="tree_summary.csv",
         mime="text/csv"
     )
-    st.dataframe(tree_summary, hide_index=True) the table")
+    st.dataframe(tree_summary, hide_index=True). Use the filters on the left to select which Quality (B: Needs Pruning, C: Needs to be cut), Lot #, and Tree Height you want displayed in the table")
 
 elif page == "SPTF Lot Map":
     st.title("SPTF Lot Map")
@@ -109,6 +109,20 @@ elif page == "SPTF Lot Map":
 elif page == "Tree Maintenance":
     st.title("Tree Maintenance Table")
     st.write("Use this page to download a printable table for Rudy. Use the filters on the left to select which Quality (B: Needs Pruning, C: Needs to be cut), Lot #, and Tree Height you want displayed in the table")
+    
+    # Filtered summary of trees
+    tree_summary = filtered_data.groupby(["Quality", "Lot", "Row", "Tree Height (ft)"])['Count'].sum().reset_index()
+    tree_summary["Work Completed?"] = ""
+    
+    # Display table
+    csv = tree_summary.to_csv(index=False).encode('utf-8')
+    st.download_button(
+        label="Download Table as CSV",
+        data=csv,
+        file_name="tree_summary.csv",
+        mime="text/csv"
+    )
+    st.dataframe(tree_summary, hide_index=True). Use the filters on the left to select which Quality (B: Needs Pruning, C: Needs to be cut), Lot #, and Tree Height you want displayed in the table")
     
     # Filtered summary of trees
     tree_summary = filtered_data.groupby(["Quality", "Lot", "Row", "Tree Height (ft)"])['Count'].sum().reset_index()
