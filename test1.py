b661e18ac183e97cdeb9eb9a0a6c170b174358c7
msmoore18 <slater.meagan@gmail.com>
2025-03-05 21:06:56 -0700

    Update test1.py

--- test1.py
+++ test1.py
@@ -122,6 +122,20 @@ elif page == "Tree Maintenance":
         file_name="tree_summary.csv",
         mime="text/csv"
     )
+    st.dataframe(tree_summary, hide_index=True). Use the filters on the left to select which Quality (B: Needs Pruning, C: Needs to be cut), Lot #, and Tree Height you want displayed in the table")
+    
+    # Filtered summary of trees
+    tree_summary = filtered_data.groupby(["Quality", "Lot", "Row", "Tree Height (ft)"])['Count'].sum().reset_index()
+    tree_summary["Work Completed?"] = ""
+    
+    # Display table
+    csv = tree_summary.to_csv(index=False).encode('utf-8')
+    st.download_button(
+        label="Download Table as CSV",
+        data=csv,
+        file_name="tree_summary.csv",
+        mime="text/csv"
+    )
     st.dataframe(tree_summary, hide_index=True)
     
     # Filtered summary of trees
