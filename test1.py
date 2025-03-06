7f6d87a1f0094388efa72f38fe7dcb394995d418
msmoore18 <slater.meagan@gmail.com>
2025-03-05 21:10:34 -0700

    Update test1.py

--- test1.py
+++ test1.py
@@ -54,18 +54,5 @@
     )
     st.dataframe(tree_summary, hide_index=True)
     
-    # Filtered summary of trees
-    tree_summary = filtered_data.groupby(["Quality", "Lot", "Row", "Tree Height (ft)"])['Count'].sum().reset_index()
-    tree_summary["Work Completed?"] = ""
     
-    # Display table
-    csv = tree_summary.to_csv(index=False).encode('utf-8')
-    st.download_button(
-        label="Download Table as CSV",
-        data=csv,
-        file_name="tree_summary.csv",
-        mime="text/csv"
-    )
-    st.dataframe(tree_summary, hide_index=True)
-    
-    
+
