03d69f1dd6665c09b8af8b4cc83304eb4b52d3a7
msmoore18 <slater.meagan@gmail.com>
2025-03-05 20:54:28 -0700

    Update test1.py

--- test1.py
+++ test1.py
@@ -78,6 +78,8 @@ if page == "Tree Inventory":
         height_bins = pd.cut(filtered_data["Tree Height (ft)"], bins=[0, 5, 10, 15, 20, float('inf')], labels=["0-5ft", "6-10ft", "11-15ft", "16-20ft", ">20ft"])
         height_distribution = filtered_data.groupby(height_bins)["Count"].sum().reset_index()
         fig = px.pie(height_distribution, names="Tree Height (ft)", values="Count")
+        fig.update_layout(showlegend=False)
+        fig.update_traces(textinfo='label+percent', insidetextorientation='radial')
         st.plotly_chart(fig)
 
     with col2:
