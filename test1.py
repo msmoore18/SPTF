7259636e4fcffd518d65119b9f2d8f8430e7e910
msmoore18 <slater.meagan@gmail.com>
2025-03-19 16:39:39 -0600

    Update test1.py

--- test1.py
+++ test1.py
@@ -173,4 +173,5 @@ elif page == "Tree Maintenance":
     st.dataframe(tree_summary, hide_index=True)
     st.markdown('</div>', unsafe_allow_html=True)
 
+st.write("Unique Quality Values in Data:", data["Quality"].unique())
 
