732bf4bfaf6d6fbf035c28d5739df33ccd896875
msmoore18 <slater.meagan@gmail.com>
2025-05-09 10:44:47 -0600

    Update sptf.py

--- sptf.py
+++ sptf.py
@@ -206,11 +206,10 @@ elif page == "Historical Sales":
 
         if not any_pre_2023:
             fig = px.pie(
-                sales_filtered.groupby("Tree Height (ft)")["Revenue"].sum().reset_index(),
-                names="Tree Height (ft)",
+                sales_filtered.groupby("Customer")["Revenue"].sum().reset_index(),
+                names="Customer",
                 values="Revenue",
-                title="Revenue Distribution by Tree Height"
-            )
+                title="Revenue Distribution by Customer"
             fig.update_traces(textinfo='percent+value', texttemplate='%{percent} <br> $%{value:,.0f}')
             st.plotly_chart(fig)
 
